735c2d158fcfe9be688aa8851be0cc9927db445a
Paulo <paulo.britoouril@gmail.com>
2026-08-26 21:32:06 -0300

    progresso_aula_2

--- lib/main.dart
+++ lib/main.dart
@@ -9,16 +9,87 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: Colors.yellow,
+        scaffoldBackgroundColor: const Color.fromARGB(255, 28, 191, 197),
       ),
       title: 'Lista de Tarefas',
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text('Ainsitezinho')
+
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    //Método responsável por construir a interface do usuário da página inicial.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Lista de Tarefas',
+          style: TextStyle(
+            color: const Color.fromARGB(255, 0, 0, 0),
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Digite uma tarefa',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8.0),
+                ElevatedButton(
+                  onPressed: () {
+                    // Lógica para adicionar a tarefa
+                  },
+                  child: const Text('Adicionar'),
+                ),
+              ],
+            ),
+            SizedBox(height: 16.0),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 10, // Substitua pelo número de tarefas
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: ListTile(
+                      title: Text('Tarefa ${index + 1}'),
+                      trailing: IconButton(
+                        icon: Icon(Icons.delete),
+                        onPressed: () {
+                          // Lógica para remover a tarefa
+                        },
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
